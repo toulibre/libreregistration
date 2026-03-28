bf3aaff2e72e5f2e1ff95ce73cc14db3a7be4577
Lionel Porcheron <lionel.porcheron@bleemeo.com>
2026-03-28 13:26:39 +0100

    feat: update screenshots

--- scripts/screenshots/main.go
+++ scripts/screenshots/main.go
@@ -248,6 +248,7 @@ func startServer(cfg *config.Config, auth *services.AuthService, events *service
 	r.Use(middleware.Session(sessionStore))
 	r.Use(middleware.Locale)
 	r.Use(middleware.CSRF([]byte(cfg.CSRFKey), false))
+	r.Use(middleware.LoadUser)
 
 	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	r.Handle("/uploads/*", http.StripPrefix("/uploads/", http.FileServer(http.Dir(uploadDir))))
